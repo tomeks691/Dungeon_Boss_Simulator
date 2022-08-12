94a6867d03bf01bb7dce78a3fff8ba2ccee5765c
lucek <luci6969@protonmail.ch>
2022-08-12 18:33:13 +0200

    Add rooms generator

--- game.py
+++ game.py
@@ -35,7 +35,7 @@ while game_on:
         rooms.append(DungeonCreator(f"room_{int(room_number) + 1}"))
     for room in rooms:
         print("Komnaty do zakupu:\n\n")
-        print(room.background_room)
+        print(room.update())
         print(f"Koszt lochu {room.cost}")
         print(f"Obrażenia lochu {room.damage}")
         print("Efekt Specjalny lochu:")
@@ -47,39 +47,53 @@ while game_on:
     while True:
         print(
             "Jeśli wybrałeś swój nowy loch wpisz odpowiedni numer pokoju (room_1 etc.) jeśli chcesz podejrzeć pokój jeszcze raz wpisz p_NumerPokoju:")
+        for i in rooms:
+            print(i.name_room)
         choice = input()
-        if choice[0] == "p":
-            preview_room = rooms[int(choice[-1])-1]
-            print("Komnaty do zakupu:\n\n")
-            print(preview_room.background_room)
-            print(f"Koszt lochu {preview_room.cost}")
-            print(f"Obrażenia lochu {preview_room.damage}")
-            print("Efekt Specjalny lochu:")
-            print(preview_room.special_effect["Description"], preview_room.special_effect["Type"], preview_room.special_effect["Stat"], sep="\n")
-            print("Jeśli chcesz wrócić do wcześniejszego menu wpisz \'back\'")
-            choice = input()
-            if choice == "back":
-                os.system("clear")
-                continue
+        if len(choice) > 0:
+            if choice[0] == "p":
+                preview_room = rooms[int(choice[-1])-1]
+                print("Komnaty do zakupu:\n\n")
+                print(preview_room.update())
+                print(f"Koszt lochu {preview_room.cost}")
+                print(f"Obrażenia lochu {preview_room.damage}")
+                print("Efekt Specjalny lochu:")
+                print(preview_room.special_effect["Description"], preview_room.special_effect["Type"], preview_room.special_effect["Stat"], sep="\n")
+                print("Jeśli chcesz wrócić do wcześniejszego menu wpisz \'back\'")
+                for i in rooms:
+                    print(i.name_room)
+                choice = input()
+                if choice == "back":
+                    os.system("clear")
+                    continue
 
-            os.system("clear")
-        elif choice[:5] == "room_":
-            try:
+                os.system("clear")
+            elif choice[:5] == "room_":
                 if rooms[int(choice[5:]) - 1].cost <= coins:
                     player_rooms.append(rooms[int(choice[5:]) - 1])
+                    rooms.pop(int(choice[5:]) - 1)
+                    for room in rooms:
+                        room.name_room = f"room_{int(room.name_room[5:])-1}"
                     print(f"Zakupiłeś {choice}")
+                    print(f"Pozostały do zakupu {rooms[0].name_room}, {rooms[1].name_room}")
+                    print(player_rooms[0].update(), player_rooms[0].special_effect, player_rooms[0].damage)
                     break
                 else:
                     print("Nie stać cię na ten pokój!")
-            except:
                 print("Wprowadziłeś numer pokoju który nie istnieje.")
                 time.sleep(1)
                 os.system("clear")
                 continue
+            else:
+                print("Wprowadziłeś nie poprawne dane.")
+                time.sleep(1)
+                os.system("clear")
+                continue
         else:
-            print("Wprowadziłeś nie poprawne dane.")
+            print("Wprowadz jakis znak!")
             time.sleep(1)
             os.system("clear")
+            continue
 
 
 
